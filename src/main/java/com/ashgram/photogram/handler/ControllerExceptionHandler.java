88635634cfca375009702ba045aa5cd64d326683
Ahram-Jeong <ahram1215@gmail.com>
2023-08-14 06:28:13 +0900

    20230814 - 회원가입 기능 : 공통 응답 Script 생성

--- src/main/java/com/ashgram/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/ashgram/photogram/handler/ControllerExceptionHandler.java
@@ -1,19 +1,22 @@
 package com.ashgram.photogram.handler;
 
 import com.ashgram.photogram.handler.ex.CustomValidationException;
-import com.ashgram.photogram.web.dto.CMRespDto;
+import com.ashgram.photogram.util.Script;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController // return을 위해 사용
 @ControllerAdvice // Controller에서 발생한 모든 Exception을 한 곳에서 처리하고 관리하는 어노테이션
 public class ControllerExceptionHandler {
+    /**
+     * 반환 타입 : CMRespDto vs Script
+     * 1. 클라이언트에게 응답 -> Script
+     * 2. Ajax 통신, Android 통신 -> CMRespDto
+     */
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e) {
-        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap()); // CustomValidationException 발생 시, CMRespDto 반환 후 종료
+    public String validationException(CustomValidationException e) {
+        return Script.back(e.getErrorMap().toString());
     }
 }
